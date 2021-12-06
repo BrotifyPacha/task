dab4bd17fe046d41994c34f36deb3dec13a257e3
Pavel Gusev <pgusev@ooo-ntl.ru>
2021-12-06 18:02:06 +0300

    Remove lock file mechanizm from on-exit hook

--- hooks/on-exit-start-sync.sh
+++ hooks/on-exit-start-sync.sh
@@ -2,15 +2,9 @@
 
 LOCK_FILE=~/.config/task/autosync.lock
 
-if [[ ! -f $LOCK_FILE ]]; then
-    touch $LOCK_FILE
-
-    # Only sync, if the backlog is not empty
-    if [[ `cat ~/.config/task/backlog.data | wc -l` > 1 ]]; then
-        task sync >> ~/.config/task/sync_hook.log
-    fi
-
-    rm $LOCK_FILE
+# Only sync, if the backlog is not empty
+if [[ `cat ~/.config/task/backlog.data | wc -l` > 1 ]]; then
+    task sync >> ~/.config/task/sync_hook.log
 fi
 
 exit 0
